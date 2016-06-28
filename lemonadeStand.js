db0cdab67f4fd62ad7028e6a573f5d48fca860f1
Jonathon Weisse <jonathonweisse@gmail.com>
2016-06-28 11:18:57 -0500

    added strategy comments to lemonadeStand.js

--- lemonadeStand.js
+++ lemonadeStand.js
@@ -36,3 +36,20 @@ var myBiz = new LemonadeStand();
 myBiz.generatePeople(20);
 myBiz.logArray(myBiz.passerbys);
 
+
+
+
+
+
+/* The Run Simulation Button needs to:
+	1. Trigger the getForecast Function based on numberOfDays Input value
+	2. Generate Passerbys based on numberOfDays input value and averageNumOfPasserbys input value
+	3. Calculate the probability of each person purchasing a lemonade (if(>75)Prob*(rand/price %75) else rand/price * 1 )
+	4. Generate a table that includes:
+		a. Day #
+		b. # of passerbys
+		c. # of lemonades sold
+		d. Profit (number of lemonades sold * (price - cost to make))
+
+*/
+
