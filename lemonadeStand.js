2a0b6933e3cdb164c7a5a0fa0b9593f812f9f102
Jonathon Weisse <jonathonweisse@gmail.com>
2016-06-28 09:44:00 -0500

    Created person and lemonadeStand classes

--- lemonadeStand.js
+++ lemonadeStand.js
@@ -1,5 +1,28 @@
-function startLemonadeStand(){
+/*function startLemonadeStand(){
 	console.log(document.weather);
+}*/
+
+function Person(){
+	this.probabilityOfPurchase = Math.random();
 }
 
-console.log(weather.forecast[1].high);
+function LemonadeStand(){
+	this.passerbys = [];
+	this.generatePeople = function(num, callback) {
+		for(var i = 0; i < num; i++){
+			callback(new Person());
+		}
+	}
+	this.pushToPasserbysArray = function(object){
+		this.passerbys.push(object);
+	}
+	this.logArray = function(array){
+		for(var i = 0; i < array.length; i++){
+			console.log(array[i].probabilityOfPurchase);
+		}
+	}
+}
+
+var myBiz = new LemonadeStand();
+myBiz.generatePeople(20,myBiz.pushToPasserbysArray);
+myBiz.logArray(myBiz.passerbys);
